dac36aba189eda484910959e257ee218c2942698
NensAleksa <108027067+NensAleksa@users.noreply.github.com>
2022-10-04 20:57:37 +0300

    created void SecondArrayWithIF

---
+++ Task/Program.cs
@@ -0,0 +1,18 @@
+﻿string[] array1 = new string[5] {"123", "2", "hello", "world", "Moscow"};
+string[] array2 = new string[array1.Length];
+
+SecondArrayWithIF(array1, array2);
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int number = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[number] = array1[i];
+        number++;
+        }
+    }
+}
+Console.WriteLine("Числа <=3: ");
